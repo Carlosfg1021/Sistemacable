a1c27888f27b2fa099bad668c6753e4056a6b43b
Carlos Daniel Franco Pérez <carlosfg1021@gmail.com>
2020-11-27 09:53:04 -0600

    migraciones

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -149,7 +149,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <a href="index3.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">Cable Robles</span>
+      <span class="brand-text font-weight-light">Cablevision Robles</span>
     </a>
 
     <!-- Sidebar -->
@@ -160,7 +160,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Victor Robles</a>
         </div>
       </div>
 
